f669f130ad4e0e991282de11d81aa98ef994ae91
Unclesmol <ntsako.khoza@yahoo.com>
2025-05-10 03:42:11 +0200

    routing fix

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/tasty-treez420">
         <App />
       </BrowserRouter>
     </ChakraProvider>
